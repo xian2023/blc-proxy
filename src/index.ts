fc162a77341657418b8bb7d83b7136cbdca32590
神代綺凛 <i@loli.best>
2023-09-24 02:47:19 +0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,90 @@
+interface ProxyOptions {
+  url: string;
+  method?: string;
+  headers?: Record<string, string>;
+}
+
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+  'Access-Control-Max-Age': '86400',
+};
+
+const allowedHostname = new Set(['blc.lolicon.app', 'localhost']);
+const allowedApis = new Set([
+  'https://api.live.bilibili.com/room/v1/Room/room_init',
+  'https://live-open.biliapi.com/v2/app/start',
+  'https://live-open.biliapi.com/v2/app/end',
+]);
+
+function resp404() {
+  return new Response(null, { status: 404 });
+}
+
+async function handleProxy({ url, method, headers }: ProxyOptions) {
+  if (!url) return resp404();
+
+  try {
+    const urlObj = new URL(url);
+    urlObj.hash = '';
+    urlObj.search = '';
+    if (!allowedApis.has(urlObj.href)) return resp404();
+  } catch {
+    return resp404();
+  }
+
+  let res = await fetch(url, { method, headers });
+  res = new Response(res.body, res);
+  res.headers.set('Access-Control-Allow-Origin', '*');
+  res.headers.set('Cache-Control', 'no-cache');
+  res.headers.delete('Set-Cookie');
+
+  return res;
+}
+
+function handleOptions(request: Request) {
+  if (request.headers.get('Access-Control-Request-Method') !== null && request.headers.get('Access-Control-Request-Headers') !== null) {
+    return new Response(null, {
+      headers: {
+        ...corsHeaders,
+        'Access-Control-Allow-Headers': request.headers.get('Access-Control-Request-Headers')!,
+      },
+    });
+  } else {
+    return new Response(null, {
+      headers: {
+        Allow: 'GET, HEAD, POST, OPTIONS',
+      },
+    });
+  }
+}
+
+const handler: ExportedHandler = {
+  async fetch(request) {
+    const origin = request.headers.get('Origin');
+    if (!origin) return resp404();
+
+    try {
+      const { hostname } = new URL(origin);
+      if (!allowedHostname.has(hostname)) return resp404();
+    } catch {
+      return resp404();
+    }
+
+    if (request.method === 'OPTIONS') {
+      return handleOptions(request);
+    } else if (request.method === 'POST' && request.headers.get('Content-Type') === 'application/json') {
+      try {
+        const options = await request.json<ProxyOptions>();
+        console.warn('options', options);
+        return handleProxy(options);
+      } catch (error) {
+        return resp404();
+      }
+    } else {
+      return resp404();
+    }
+  },
+};
+
+export default handler;
